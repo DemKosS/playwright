7f1c71b4973442e1a318112c4df6a8c25c20d685
DemKosS <sidyakinkn@gmail.com>
2022-11-04 15:23:07 +0300

    init

--- NetologyTest.js
+++ NetologyTest.js
@@ -1,48 +1,47 @@
-const {  expect } = require("@playwright/test");
-const { chromium } = require("playwright");
-const { timeout } = require("./playwright.config");
-const { getEmail, getPass, getRandomE, getRandomPass } = require("./user");
-
-("Should Successful Auth", async () => {
-  const browser = await chromium.launch({
-    headless: false,
-    slowMo: 4000,
-    devtools: false
-  });
-  const page = await browser.newPage();
-
-  await page.goto("https://netology.ru/?modal=sign_in", {waitUntil: 'domcontentloaded', timeout: 45000});
-  await page.click('input[name="email"]');
-  await page.fill('input[name="email"]', getEmail());
-  await page.click('input[name="password"]');
-  await page.fill('input[name="password"]', getPass());
-  await page.click('button[data-testid="login-submit-btn"]');
-
-   //assertion
-  await expect(page.url()).toBe("https://netology.ru/profile");
-  await expect(page.locator("h2")).toHaveText("Мои курсы и профессии");
-
-  await browser.close();
-})();
-
-("Should not successful auth", async () => {
-  const browser = await chromium.launch({
-    headless: false,
-    slowMo: 4000,
-    devtools: false
-  });
-  const page = await browser.newPage();
-
-  await page.goto("https://netology.ru/?modal=sign_in", {waitUntil: 'domcontentloaded', timeout: 45000});
-  await page.click('input[name="email"]');
-  await page.fill('input[name="email"]', getRandomE());
-  await page.click('input[name="password"]');
-  await page.fill('input[name="password"]', getRandomPass());
-  await page.click('button[data-testid="login-submit-btn"]');
-
-   //assertion
-  await expect(page.locator("data-testid=login-error-hint")).toContainText("Вы ввели неправильно логин или пароль");
-  
-
-  await browser.close();
-})();
+ //Тест 1. Успешная авторизация
+ const { test } = require('@playwright/test');
+ const {User} = require("../user.js")
+ 
+ 
+ const config = {
+   headless: false,
+   timeout: 30000,
+   globalTimeout: 600000,
+   reporter: 'list',
+   testDir: './tests',
+ };
+ 
+ test.describe('two tests', () => {
+ 
+ test('Успешная авторизация', async({page}) => {
+   
+   const userInstance = new User()
+   let login =  userInstance.login;
+   let password = userInstance.password;
+   await page.goto('https://netology.ru/?modal=sign_in');
+   await page.click('[name="email"]');   
+   await page.fill('[placeholder="Email"]', login);    
+   await page.click('[name="password"]');
+   await page.fill('[placeholder="Пароль"]', password);
+   await Promise.all([
+     page.waitForNavigation(/*{ url: 'https://netology.ru/profile' }*/),
+     page.click('[data-testid="login-submit-btn"]')
+   ]);
+   await (page.locator('h2'), {hasText: 'Мои курсы и профессии'});
+     
+ });
+ 
+ //Тест 2. Неуспешная авторизация
+ test('Неуспешная авторизация', async({page}) => {
+   await page.goto('https://netology.ru/?modal=sign_in');
+   await page.click('[name="email"]');   
+   await page.fill('[placeholder="Email"]', "login@mail.ru");    
+   await page.click('[name="password"]');
+   await page.fill('[placeholder="Пароль"]', "password");
+   await page.click('[data-testid="login-submit-btn"]');
+   await (page.locator('[data-testid="login-error-hint"]'), {hasText: 'Вы ввели неправильно логин или пароль'})
+     
+ });
+ 
+ })
+ 
